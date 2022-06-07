e164e69f9a4b84b95a318edfa9c39d795acb8455
ArcziDudu <artur.augustyn26@gmail.com>
2022-06-07 20:51:18 +0200

    ready

---
+++ main.js
@@ -0,0 +1,40 @@
+const modal = document.querySelector('.modal')
+const input = document.querySelector('.modal-input')
+const modalBtn = document.querySelector('.modal-btn')
+const saveBtn = document.querySelector('.save-btn')
+const text = document.querySelector('.text')
+const errorMsg = document.querySelector('.error-msg')
+
+let inputValue = 'to jest testowa wiadomość'
+let timeout
+let index = 1
+let speed = 80
+
+const writtingAnimation = () => {
+    text.innerHTML = inputValue.slice(0, index)
+    index++
+    if(index > text.length){
+        return
+    }
+    timeout = setTimeout(writtingAnimation, speed)
+}
+
+const showModal = () =>{
+modal.classList.add('active')
+}
+const closeModal = () =>{
+    if(input.value == ''){
+        errorMsg.textContent = 'Wprowadź tekst'
+        return
+    }
+    inputValue = input.value
+    modal.classList.remove('active')
+    index = 1
+    clearTimeout(timeout)
+    writtingAnimation()
+    input.value = ''
+    errorMsg.textContent = ''
+}
+modalBtn.addEventListener('click', showModal)
+saveBtn.addEventListener('click', closeModal)
+// writtingAnimation()
